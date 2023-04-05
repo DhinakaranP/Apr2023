cd5d1c47f8bc3a61283806ec7e3f595625ad2073
DhinakaranP <sarathidhina@gmail.com>
2023-04-05 22:30:13 +0530

    Om nama shivaya

---
+++ OMOM.java
@@ -0,0 +1,20 @@
+package omnamasivaiy;
+
+public class OMOM {
+
+	public void a ()
+	{
+	
+	int b=100;
+	
+	System.out.println("The Value of b is :" +b );
+	}
+	
+	
+	public static void main(String[] args) {
+		
+		OMOM object=new OMOM();
+		
+		object.a();
+	}
+}
